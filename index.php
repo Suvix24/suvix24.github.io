b07a599a6cbb9f3db038bb2f562055b67c903df3
suvix24 <adrian.lamcha@gmail.com>
2020-02-02 11:34:34 +0100

    Add firebase

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-Hello World
+<Html>
+	<head>
+	</head>
+
+	<body>
+		Hello World!
+		<!-- The core Firebase JS SDK is always required and must be listed first -->
+		<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-app.js"></script>
+
+		<!-- TODO: Add SDKs for Firebase products that you want to use
+		     https://firebase.google.com/docs/web/setup#available-libraries -->
+		<script src="https://www.gstatic.com/firebasejs/7.8.0/firebase-analytics.js"></script>
+
+		<script>
+		  // Your web app's Firebase configuration
+		  var firebaseConfig = {
+		    apiKey: "AIzaSyD8NNGamv539bTJa-l1ZpJQN-9MrZvHLJM",
+		    authDomain: "first-website-e6635.firebaseapp.com",
+		    databaseURL: "https://first-website-e6635.firebaseio.com",
+		    projectId: "first-website-e6635",
+		    storageBucket: "first-website-e6635.appspot.com",
+		    messagingSenderId: "331764603899",
+		    appId: "1:331764603899:web:53f0bce85dc60260cd095e",
+		    measurementId: "G-NHH2SHMCNJ"
+		  };
+		  // Initialize Firebase
+		  firebase.initializeApp(firebaseConfig);
+		  firebase.analytics();
+		</script>
+	</body>
+</Html>
